fac67473dee03f9f2ad30622af9de5f21d60b291
amg34km <yamagata34km@gmail.com>
2018-02-11 18:44:22 +0900

    open文の導入を行った

--- CircularMotion.f90
+++ CircularMotion.f90
@@ -1,19 +1,25 @@
 program circularMotion
   implicit none
   real,parameter::pi=3.1415926
-  real x , y , vx , vy , v0 , a , ax , t , dt
-  integer n
+  real(8) x , y , vx , vy , v0 , a , ax , t , dt
+  integer n, Openstatus
+
+  open(25,file = "result", status = "replace", action ="write",&
+  access = "sequential", position = "rewind",&
+  iostat = Openstatus)
+  if ( Openstatus > 0 ) stop "Error opening file.25"
   x=3
-  v0 = 6*pi
+  v0 = pi
   t=0
-  dt=0.08
+  dt=0.00001
+  y = 0
   write(6,*)'input acceleration'
   read(5,*)a
   write(6,*)'circular orbital'
 vx=v0
 vy=0
-  do n=1,1000
-    write(6,*)x,y
+  do n=1,1000000
+    write(25,*)x,y
     t= t + dt
 
     vx= vx - a * ( vy / sqrt( vx**2 + vy**2 ) ) * dt
@@ -21,5 +27,7 @@ vy=0
     vy= vy + a * ( vx / sqrt( vx**2 + vy**2 ) ) * dt
     y= y + vy * dt
   end do
+
+  close(25)
 
 endprogram circularMotion
